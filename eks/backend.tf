05cc284845d9a6e84799c9f2188c85d205a380c9
Anshuman Mishra <63114740+anshumanmishr@users.noreply.github.com>
2024-09-24 19:25:47 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-bucket1-am"
+    bucket         = "terraform-bucket-am"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     #dynamodb_table = "Lock-Files"
